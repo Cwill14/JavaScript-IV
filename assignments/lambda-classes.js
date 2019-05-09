583c887e1619f857c26aa7d7f9fc7d23a228bc28
Clark Williams <theclarkwilliams@gmail.com>
2019-05-09 15:39:16 -0600

    tests work, MVP

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -6,11 +6,11 @@ class Person {
         this.location = attributes.location;        
     }
     speak() {
-        console.log(`Hello, my name is ${this.name}, I amd from ${this.location}`);
+        console.log(`Hello, my name is ${this.name}, I am from ${this.location}`);
     }
 }
 
-class Instructors extends Person {
+class Instructor extends Person {
     constructor(iAttributes) {
         super(iAttributes);
         this.specialty = iAttributes.specialty;
@@ -20,12 +20,12 @@ class Instructors extends Person {
     demo(subject) {
         console.log(`Today we are learning about ${subject}`);
     }
-    grade(studentObj, subject) {
+    grade(student, subject) {
         console.log(`${student.name} receives a perfect score on ${subject}`);
     }
 }
 
-class Students extends Person {
+class Student extends Person {
     constructor(sAttributes) {
         super(sAttributes);
         this.previousBackground = sAttributes.previousBackground;
@@ -43,16 +43,52 @@ class Students extends Person {
     }
 }
 
-class ProjectManagers extends Instructors {
+class ProjectManager extends Instructor {
     constructor(pmAttributes) {
         super(pmAttributes);
         this.gradClassName = pmAttributes.gradClassName;
         this.favInstructor = pmAttributes.favInstructor;
     }
     standUp(slackChannel) {
-        console.log(`${name} announces to ${slackChannel}, @channel standy times!`);
+        console.log(`${this.name} announces to ${slackChannel}, @channel standy times!`);
     }
-    debugsCode(studentObj, subject) {
-        console.log(`${name} debugs ${student.name}'s code on ${subject}`);
+    debugsCode(student, subject) {
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     }
 }
+
+const bill = new Instructor({
+    name: 'Bill',
+    age: '32',
+    location: 'Boston',
+    specialty: 'Front End',
+    favLanguage: 'JavaScript',
+    catchPhrase: 'Git er done!'
+})
+
+const george = new Student({
+    name: 'George',
+    age: 25,
+    location: 'Dallas',
+    previousBackground: 'Carpenter',
+    className: 'Web22',
+    favSubjects: ['HTML', 'CSS']
+})
+
+const joe = new ProjectManager({
+    name: 'Joe',
+    age: 29,
+    location: 'Louisville',
+    gradClassName: 'CS2',
+    favInstructor: 'Bill'
+})
+
+
+bill.speak();
+bill.demo('classes');
+bill.grade(george, 'Objects');
+george.listsSubjects();
+george.PRAssignment('Prototypes');                                             
+george.sprintChallenge('JavaScript III');
+joe.standUp('web22_joe');
+joe.debugsCode(george, 'Preprocessing II');
